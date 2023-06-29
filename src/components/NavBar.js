19db79d33caa43b9af0169019d40ecfd8162362b
Sakya Thilakarathne <91802025+sakya-thilakarathne@users.noreply.github.com>
2023-06-29 22:46:33 +0530

    saved with my personal details

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -50,9 +50,9 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="https://www.linkedin.com/in/sakya-thilakarathne-72a057246/"><img src={navIcon1} alt="" /></a>
+                <a href="https://www.facebook.com/sakya.thilakarathne"><img src={navIcon2} alt="" /></a>
+                <a href="https://www.instagram.com/_sakiyaa_/"><img src={navIcon3} alt="" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
